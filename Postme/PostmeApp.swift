57c1907d81bf7f5f0c4bfe01b2921a88e9f8f82a
Felix <0xfelixli@gmail.com>
2026-05-24 15:30:37 +0800

    Initial Commit

---
+++ Postme/PostmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostmeApp.swift
+//  Postme
+//
+//  Created by Felix on 2026/5/24.
+//
+
+import SwiftUI
+
+@main
+struct PostmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
